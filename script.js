e8b91b4eb1e30bf2bddba980460fa864ad808b92
Pedro H. Mariano 2-b ¯\_(ツ)_/¯ <108529901+Pedromshms@users.noreply.github.com>
2024-06-14 11:20:18 -0300

    vd

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal =document.querySelector("caixa-principal");
+const caixaPergunto =document.querySelector("caixa-pergunta");
+const caixaAlternativa =document.querySelector("caixa-alternativa");
+const caixaResultado =document.querySelector("caixa-resultado");
+const caixaResultado =document.querySelector("caixa-resultado");
