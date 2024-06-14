d0b50ec4541993520b9163038aff4287d0a4f70a
Pedro H. Mariano 2-b ¯\_(ツ)_/¯ <108529901+Pedromshms@users.noreply.github.com>
2024-06-14 11:28:08 -0300

    re

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ const caixaPergunto =document.querySelector("caixa-pergunta");
 const caixaAlternativa =document.querySelector("caixa-alternativa");
 const caixaResultado =document.querySelector("caixa-resultado");
 const caixaResultado =document.querySelector("caixa-resultado");
+
+const = perguntas[
+    {
+    enunciado: "pergunta 1",
+    alternativas: [
+        "Alternativa 1"
+        "Alternativa 2"
+    }
+    ]
+    {
+        enunciado: "pergunta 2",
+        alternativas: [
+            "Alternativa 1"
+            "Alternativa 2"
+        }
+        ]
+        
+    
+]
